f07af226a5b2706b7b9f9db3dacfab0756c70c02
jose55440 <josemanuelgarciasanchez10@yahoo.com>
2024-05-03 13:15:42 +0200

    cambios en config firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,12 +1,18 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  // Pega aquí tu configuración de Firebase
+  apiKey: "AIzaSyAqKMlTiNSecd_izbBuztxnVRB27GkgWQA",
+  authDomain: "prueba2-8bdb4.firebaseapp.com",
+  databaseURL: "https://prueba2-8bdb4-default-rtdb.firebaseio.com",
+  projectId: "prueba2-8bdb4",
+  storageBucket: "prueba2-8bdb4.appspot.com",
+  messagingSenderId: "921968605792",
+  appId: "1:921968605792:web:8a3e3550682c294ff8e105"
 };
 
-firebase.initializeApp(firebaseConfig);
-
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
