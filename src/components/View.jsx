406a047e75f175679fc24d4e7e9c6922f37544fa
jose55440 <josemanuelgarciasanchez10@yahoo.com>
2024-05-07 11:04:12 +0200

    a

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 
 export const View = () => {
   return (
-    <div>View</div>
-  )
-}
+    
+      <div>
+        <Create />
+        <MagicMotion>
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            id={task.id}
+            name={task.name}
+            completed={task.completed}
+            removeTask={removeTask}
+            toComplete={toComplete}
+          />
+        ))}
+        </MagicMotion>
+      </div>
+    
+  );
+};
