3c49c5f74f3e86bef56ff4c3a56bb84dbf7a13d5
jose55440 <josemanuelgarciasanchez10@yahoo.com>
2024-05-07 11:03:01 +0200

    a

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,39 +1,7 @@
-import React from "react";
-import { useTaskList } from "../hooks/useTaskList";
-import { Task } from "./Task";
-import { Create } from "./Create";
-import { MagicMotion } from "react-magic-motion";
+import React from 'react'
 
 export const View = () => {
-  const { tasks, removeTask, toComplete } = useTaskList();
-
   return (
-    <MagicMotion>
-      <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <MagicMotion>
-        <Create />
-        
-=======
-        <Create />
-
->>>>>>> parent of 931d207 (cambios)
-=======
-        <Create />
-
->>>>>>> parent of 931d207 (cambios)
-        {tasks.map((task) => (
-          <Task
-            key={task.id}
-            id={task.id}
-            name={task.name}
-            completed={task.completed}
-            removeTask={removeTask}
-            toComplete={toComplete}
-          />
-        ))}
-      </div>
-    </MagicMotion>
-  );
-};
+    <div>View</div>
+  )
+}
