ae39ba954e588fb6b18422317af38c9dd96575cc
jose55440 <josemanuelgarciasanchez10@yahoo.com>
2024-05-15 13:17:25 +0200

    merge

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -3,14 +3,13 @@ import { useTaskList } from "../hooks/useTaskList";
 import { Task } from "./Task";
 import { Create } from "./Create";
 import { motion } from "framer-motion";
+import { useUserSet } from "../hooks/useUserSet";
 
 export const View = () => {
-  const { tasks, removeTask, toComplete, fetchTasksFromDB } = useTaskList();
-
-  useEffect(() => {
-    fetchTasksFromDB();
-  }, [fetchTasksFromDB]);
-
+  const {tasks,removeTask,toComplete} = useTaskList()
+  const {user} = useUserSet();
+  
+   
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -20,17 +19,22 @@ export const View = () => {
     >
       <div>
         <Create />
-
-        {tasks.map((task) => (
-          <Task
-            key={task.id}
-            id={task.id}
-            name={task.name}
-            completed={task.completed}
-            removeTask={removeTask}
-            toComplete={toComplete}
-          />
-        ))}
+  
+        {tasks.map((task) => {
+          if (task.idUser === user.id) {
+            return (
+              <Task
+                key={task.id}
+                id={task.id}
+                name={task.name}
+                completed={task.completed}
+                removeTask={removeTask}
+                toComplete={toComplete}
+              />
+            );
+          }
+          return null; // Return null if condition is not met
+        })}
       </div>
     </motion.div>
   );
