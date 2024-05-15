2a4b30dca86b41ee85e977c0a5174ef964dbdedc
jose55440 <josemanuelgarciasanchez10@yahoo.com>
2024-05-15 11:31:15 +0200

    arreglau

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTaskList } from "../hooks/useTaskList";
 import { Task } from "./Task";
 import { Create } from "./Create";
@@ -10,7 +10,16 @@ export const View = () => {
   const {tasks,removeTask,toComplete} = useTaskList()
   const {user} = useUserSet();
   
-   
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetchTasks();
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -21,7 +30,7 @@ export const View = () => {
       <div>
         <Create />
   
-        {tasks.map((task) => {
+        {data.map((task) => {
           if (task.idUser === user.id) {
             return (
               <Task
